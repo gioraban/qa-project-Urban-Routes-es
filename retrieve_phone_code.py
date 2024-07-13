f73c9617bb1af08a33f9b6cd5001363dc8dffb1c
gioraban <gkhamisso@gmail.com>
2024-07-12 21:00:31 -0600

    Create retrieve_phone_code.py

---
+++ retrieve_phone_code.py
@@ -0,0 +1,23 @@
+
+#Esta funcion devuelve el codigo sms que recive el Usuario luego de agregar su nuemero de telefono
+def retrieve_phone_code(driver) -> str:
+    import json
+    import time
+    from selenium.common import WebDriverException
+    code = None
+    for i in range(10):
+        try:
+            logs = [log["message"] for log in driver.get_log('performance') if log.get("message")
+                    and 'api/v1/number?number' in log.get("message")]
+            for log in reversed(logs):
+                message_data = json.loads(log)["message"]
+                body = driver.execute_cdp_cmd('Network.getResponseBody',
+                                              {'requestId': message_data["params"]["requestId"]})
+                code = ''.join([x for x in body['body'] if x.isdigit()])
+        except WebDriverException:
+            time.sleep(2)
+            continue
+        if not code:
+            raise Exception("No se encontró el código de confirmación del teléfono.\n"
+                            "Utiliza 'retrieve_phone_code' solo después de haber solicitado el código en tu aplicación.")
+        return code
