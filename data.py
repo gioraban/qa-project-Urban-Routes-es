09507f21e9391566d2e5acad85afda9db764a153
gioraban <gkhamisso@gmail.com>
2024-07-12 20:47:29 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,14 +1,14 @@
-#URL de la pagina web Urban Routes
-urban_routes_url = 'https://cnt-34fab35d-31a4-4d3b-b53b-e7476d0a3dfd.containerhub.tripleten-services.com/?lng=es'
+#URL inicial de la pagina web Urban Routes
+URBAN_ROUTES_URL = 'https://cnt-65c5a187-a108-41c1-a778-877b283f544c.containerhub.tripleten-services.com/?lng=es'
 #Direccion para la  casilla Desde
-address_from = 'East 2nd Street, 601'
+ADDRESS_FROM = 'East 2nd Street, 601'
 #Direccion para la casilla Hasta
-address_to = '1300 1st St'
+ADDRESS_TO = '1300 1st St'
 #Numero telefonico del Usuario
-phone_number = '+1 123 123 12 12'
+PHONE_NUMBER = '+1 123 123 12 12'
 #Numero de la tarjeta de credito del Usuario
-card_number = '1234 5678 9100'
+CARD_NUMBER = '1234 5678 9100'
 #Numero del codigo tarjeta del Usuario.
-card_code = '111'
+CARD_CODE = '111'
 #Mensaje para el conductor.
-message_for_driver = 'Drive me to Central Park'
+MESSAGE_DRIVER = 'Drive me to Central Park'
